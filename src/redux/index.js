39f6f3f3b3b572af1b9a0ff88381a163fa03ae00
Jackie Fan <fjqqiqi@163.com>
2020-11-17 21:49:03 +0800

    Remove the apiSaga importing

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,19 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/index';
 import { forbiddenWordsMiddleware } from '../middleware';
 import createSagaMiddleware from 'redux-saga';
-import apiSaga from '../sagas/api-saga';
-
-const initialiseSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   storeEnhancers(
-    applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware)
+    applyMiddleware(forbiddenWordsMiddleware)
   )
 );
-
-initialiseSagaMiddleware.run(apiSaga);
 
 export default store;
