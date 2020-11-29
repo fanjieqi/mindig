a667b9040be79f9cdf5d289fb1cd24bcc4652514
Jackie Fan <fjqqiqi@163.com>
2020-11-29 21:17:23 +0800

    Use Copyright icon in MyFooter

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { CopyrightOutlined } from '@ant-design/icons';
 const { Footer } = Layout;
 var moment = require('moment'); 
 
 const MyFooter = () => (
-  <Footer style={{ position: 'fixed', zIndex: 1, width: '100%', background: '#fff', bottom: 0, textAlign: 'center' }}>Mindig ©{moment().year()} Created by Jackie Fan</Footer>
+  <Footer style={{ position: 'fixed', zIndex: 1, width: '100%', background: '#fff', bottom: 0, textAlign: 'center' }}>Mindig <CopyrightOutlined />{moment().year()} Created by Jackie Fan</Footer>
 );
 
 export default MyFooter;
